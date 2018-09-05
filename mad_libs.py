f87566e437698aabf0344e5425d92f02e0de4290
Justin Lane <jclane@users.noreply.github.com>
2018-09-05 14:42:06 -0400

    Create mad_libs.py

---
+++ mad_libs.py
@@ -0,0 +1,12 @@
+
+
+while True:
+
+    adj1 = input("Please enter an adjective >> ")
+    adj2 = input("PLease enter another adjective >> ")
+    noun1 = input("Please enter a noun >> ")
+    verb = input("Please enter a verb >> ") 
+    adj3 = input("Please enter an adjective >> ")
+    noun2 = input("Please enter a noun >> ")
+    
+    print("The {} {} {} {} over the {} {}.".format(adj1, adj2, noun1, verb, adj3, noun2))
