ce56f0e8281faccb915caa65e42c8e1db8745839
Justin Lane <jclane@users.noreply.github.com>
2018-09-05 14:27:47 -0400

    working

--- change_calculator.py
+++ change_calculator.py
@@ -1,4 +1,9 @@
-COINS = {"quarters":.25, "nickles":.05, "dimes":.10, "pennies":.01}
+from decimal import Decimal, ROUND_HALF_UP
+
+COINS = {"quarters":Decimal('0.25'), "nickles":Decimal('0.05'), "dimes":Decimal('0.10'), "pennies":Decimal('0.01')}
+
+def round_decimal(x):
+  return x.quantize(Decimal(".01"), rounding=ROUND_HALF_UP)
 
 while True:
     coins_due = []
@@ -6,23 +11,24 @@ while True:
     def get_coins(coin):
         global num
         count = 0
-        result = 0
+        result = Decimal('0.00')
         while result < num:
-            result += COINS[coin]
+            result = result + Decimal(COINS[coin])
             count += 1
             
             if result == num:
-                num -= COINS[coin] * count
+                num -= Decimal(COINS[coin]) * count
                 return count
             elif result > num:
                 count -= 1
-                result -= COINS[coin]
-                num -= COINS[coin] * count
+                result = result - Decimal(COINS[coin])
+                num -= Decimal(COINS[coin]) * count
                 return count
 
-        
-
-    num = float(input("\nEnter change due >> "))
+    tendered = Decimal(input("\nEnter amount tendered >> "))
+    price = Decimal(input("\nEnter price of item >> "))
+    
+    num = round_decimal(tendered - price)
 
     quarters = get_coins("quarters")
     dimes = get_coins("dimes")
@@ -31,6 +37,6 @@ while True:
     
     print("\nChange due:")
     print("\n\tQuarter: " + str(quarters))
-    print("\tDimes: " + str(dimes))
+    print("\tDimes:   " + str(dimes))
     print("\tNickles: " + str(nickles))
     print("\tPennies: " + str(pennies))
