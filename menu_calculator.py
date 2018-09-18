ca9a0f075daf14956767d54695b75956d298a60c
Justin Lane <jclane@users.noreply.github.com>
2018-09-18 15:16:51 -0400

    Update menu_calculator.py

--- menu_calculator.py
+++ menu_calculator.py
@@ -1,10 +1,36 @@
-MENU = {"1":3.50, "2":2.50, "3":4.00, "4":3.50, "5":1.75, "6":1.50, "7":2.25, "8":3.75, "9":1.25}
+from sys import exit
+from os import system
+
+
+MENU = {"1":("Chicken Strips", 3.50), "2":("French Fries", 2.50), "3":("Hambuger", 4.00),
+        "4":("Hotdog", 3.50), "5":("Large Drink", 1.75), "6":("Medium Drink", 1.50), 
+        "7":("Milk Shake", 2.25), "8":("Salad", 3.75), "9":("Small Drink", 1.25)}
 
 while True:
     total = 0
-    order = input(">> ")
+    order_counts = {}
+    
+    print("\nMENU:\n")
+
+    for item in MENU:
+        print("{}. {:<16} ${:<16,.2f}".format(item, MENU[item][0], MENU[item][1]))
+
+    order = input("\n>> ")
     
     for num in order:
-        total += MENU[num]
+        count = 0
+        total += MENU[num][1]
+        order_counts[num] = order.count(num)
         
-    print("TOTAL DUE: ${:,.2f}".format(total))
+    print("\nOrder Summary:\n")
+
+    for count in order_counts:
+        print("\t" + str(order_counts[count]), MENU[count][0])
+    
+    
+    print("\nTOTAL DUE: ${:,.2f}\n".format(total))
+    response = input("New order? >> [Y/n] ")
+    if response.lower() == "n":
+        exit()
+    else:
+        system('cls||echo -e \\\\033c')
