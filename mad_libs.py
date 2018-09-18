6afdb39dba53a15be63e1f135497185ba99be503
Justin Lane <jclane@users.noreply.github.com>
2018-09-18 14:08:30 -0400

    Update mad_libs.py

--- mad_libs.py
+++ mad_libs.py
@@ -1,12 +1,14 @@
 
 
-while True:
+adj1 = input("Please enter an adjective >> ")
+adj2 = input("PLease enter another adjective >> ")
+noun1 = input("Please enter a noun >> ")
+verb = input("Please enter a verb >> ") 
+adj3 = input("Please enter an adjective >> ")
+noun2 = input("Please enter a noun >> ")
+name = input("Please enter a name >> ")
 
-    adj1 = input("Please enter an adjective >> ")
-    adj2 = input("PLease enter another adjective >> ")
-    noun1 = input("Please enter a noun >> ")
-    verb = input("Please enter a verb >> ") 
-    adj3 = input("Please enter an adjective >> ")
-    noun2 = input("Please enter a noun >> ")
-    
-    print("The {} {} {} {} over the {} {}.".format(adj1, adj2, noun1, verb, adj3, noun2))
+name = name.title()
+
+print("The {} {} {} {} over the {} {}.".format(adj1, adj2, noun1, verb, adj3, noun2))
+print("I {} {}, but I didn't {} the {}.".format(verb, name, verb, noun2))
