67d27126d8314b126a4f8911e1d8a7fecc8ed7bb
jclane <justin@justinlane.me>
2018-10-07 15:25:28 -0400

    Fixed file not found error.

--- til_facts.py
+++ til_facts.py
@@ -1,4 +1,6 @@
-import requests, csv
+import requests
+import csv
+import os.path
 from time import sleep
 
 
@@ -11,6 +13,9 @@ class Fact:
         
         if self.title.startswith("about"):
             self.title = self.title.replace("about", "", 1).strip()
+
+        if self.title.startswith("that"):
+            self.title = self.title.replace("that", "", 1).strip()
             
         if self.title.startswith(","):
             self.title = self.title.replace(",", "", 1).strip()
@@ -28,37 +33,50 @@ class Fact:
         print("\n" + self.title)
         print("\n" + self.url + "\n")
 
-def save_fact(data):
-    file = r"C:\Users\a803415\Desktop\facts.csv"
-    
-    with open(file, 'a', newline='', encoding='latin1') as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(data)
 
-def check_for_duplicates(data):
-    file = r"C:\Users\a803415\Desktop\facts.csv"
-    
-    with open(file, "r", encoding="latin1") as csvfile:
-        reader = csv.reader(csvfile, quotechar='"')
-        for row in reader:
-            if data == row[0]:
-                return False
+def save_fact(data):
+    file_path = "facts.csv"
+
+    if os.path.exists(file_path):
+        with open(file_path, "a", newline='', encoding="UTF8") as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(data)
+    else:
+        with open(file_path, "w", newline='', encoding="UTF8") as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(data)
+
+
+def check_for_duplicates(title):
+    file_path = "facts.csv"
+
+    if os.path.exists(file_path):
+        with open(file_path, "r", encoding="UTF8") as csvfile:
+            reader = csv.reader(csvfile, quotechar='"')
+            for row in reader:
+                if title == row[0]:
+                    return True
+                else:
+                    continue
+    else:
+        return False
         
+
 def get_fact():
     url = "https://www.reddit.com/r/todayilearned/new/.json"
-    resp = requests.get(url, 
-                        verify=False, headers={'User-agent':'Simple Test'})
+    resp = requests.get(url, headers={'User-agent': 'Simple Test'})
     data = resp.json()
     
     if 'error' not in data.keys():
         return data
     else:
         print("Error fetching fact.")
-        
+
+
 while True:
     fact = Fact(get_fact())
         
-    if check_for_duplicates(fact.title) != False:
+    if not check_for_duplicates(fact.title):
         fact.show()
         save_fact([fact.title, fact.url])
         
