b65bc1362d4e2bd2e0e8e12386fb31874c546528
Justin Lane <jclane@users.noreply.github.com>
2020-03-11 11:45:19 -0400

    C++ formatting

--- NinetyNineBottles.cpp
+++ NinetyNineBottles.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 
-int main() {
-  int n = 99;
+int ninetyNineBottles() 
+{
+  int bottles{99};
 
-  while (n > 0) {
-    printf("%d %s of beer on the wall! Take one down, pass it around.\n",
-            n, n > 1 ? "bottles":"bottle");
-    n--;
+  while (bottles > 0) 
+  {
+    printf("\n%d %s of beer on the wall!\nTake one down, pass it around.\n",
+                 bottles, 
+                 bottles > 1 ? "bottles":"bottle");
+    --bottles;
   }
 
   std::cout << "\nOh, no!  We're all out of beer!\n";
