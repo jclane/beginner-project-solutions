899a0a15775a6ff7499f3fa473c57dcf9e1caba9
jclane <justin@justinlane.me>
2020-07-31 15:56:49 +0000

    created folders for each language

--- chickens_rabbits.py
+++
@@ -1,9 +0,0 @@
-
-
-def count_animals(heads, legs, count):
-    while not (2 * count) + (4 * (heads - 1)) == legs:
-        return count_animals(heads - 1, legs, count + 1)
-    return count, heads - 1
-
-
-print(count_animals(36, 94, 0))
